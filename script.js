c29e996f881fb13125866b488ad6fd91a6d40e3c
SNFSoul <devsnf@gmail.com>
2026-07-05 01:09:57 -0500

    version v1.0

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Welcome to SNFSoul's Portfolio!");
+window.addEventListener("load", function () {
+
+    VANTA.NET({
+        el: "#background",
+
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+
+        color: 0x8b5cf6,
+        backgroundColor: 0x090909,
+
+        points: 10,
+        maxDistance: 25,
+        spacing: 32,
+
+        showDots: true
+    });
+
+});
+
+window.addEventListener("scroll", function(){
+
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+
+        nav.style.background = "rgba(15,15,20,.9)";
+
+    }else{
+
+        nav.style.background = "rgba(15,15,20,.55)";
+
+    }
+
+});
